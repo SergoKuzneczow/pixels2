13797cac29bf7408b775ddd05cadec8b98cabd7e
Sergo <sergokuzneczow@gmail.com>
2025-10-03 13:01:59 +0300

    Теперь для PixelsSurfaceButton можно добавить иконку.

--- core/ui/src/main/kotlin/com/sergokuzneczow/core/system_components/buttons/PixelsSurfaceButton.kt
+++ core/ui/src/main/kotlin/com/sergokuzneczow/core/system_components/buttons/PixelsSurfaceButton.kt
@@ -1,13 +1,14 @@
 package com.sergokuzneczow.core.system_components.buttons
 
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
@@ -32,6 +34,8 @@ public fun PixelsSurfaceButton(
     enabled: Boolean = true,
     contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
     interactionSource: MutableInteractionSource? = null,
+    icon: ImageVector? = null,
+    iconContent: @Composable () -> Unit = {}
 ) {
     val interactionSource = interactionSource ?: remember { MutableInteractionSource() }
     Surface(
@@ -45,26 +49,41 @@ public fun PixelsSurfaceButton(
         shadowElevation = 1.dp,
         interactionSource = interactionSource
     ) {
-        Row(
-            Modifier
-                .defaultMinSize(
-                    minWidth = ButtonDefaults.MinWidth,
-                    minHeight = ButtonDefaults.MinHeight
-                )
-                .padding(contentPadding),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically,
-            content = {
-                Text(
-                    text = text,
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.onSurface,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .align(Alignment.CenterVertically)
-                )
-            }
-        )
+        Box {
+            Row(
+                content = {
+                    icon?.let {
+                        Icon(
+                            imageVector = it,
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    }
+                    iconContent.invoke()
+                },
+                modifier = Modifier
+                    .padding(start = Dimensions.Padding)
+                    .align(Alignment.CenterStart)
+            )
+            Row(
+                Modifier
+                    .defaultMinSize(
+                        minWidth = ButtonDefaults.MinWidth,
+                        minHeight = ButtonDefaults.MinHeight
+                    )
+                    .padding(contentPadding)
+                    .align(Alignment.Center),
+                content = {
+                    Text(
+                        text = text,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
+                }
+            )
+        }
     }
 }
 
