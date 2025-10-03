2031a00d53230703c7cc6f8ad3302d2a46b36693
Sergo <sergokuzneczow@gmail.com>
2025-10-03 11:38:08 +0300

    Теперь ImageLoaderApi предоставляет метод, возвращающий bitmap или ошибку в лямюду при выполнении.

--- core/data/src/main/kotlin/com/sergokuzneczow/repository/api/ImageLoaderApi.kt
+++ core/data/src/main/kotlin/com/sergokuzneczow/repository/api/ImageLoaderApi.kt
@@ -1,8 +1,16 @@
 package com.sergokuzneczow.repository.api
 
+import android.graphics.Bitmap
 import coil3.ImageLoader
 
 public interface ImageLoaderApi {
 
     public val imageLoader : ImageLoader
+
+    public suspend fun loadBitmap(
+        path: String,
+        onStart: () -> Unit,
+        onSuccess: (bitmap: Bitmap) -> Unit,
+        onError: (throwable: Throwable) -> Unit
+    )
 }
--- core/data/src/main/kotlin/com/sergokuzneczow/repository/impl/ImageLoaderImpl.kt
+++ core/data/src/main/kotlin/com/sergokuzneczow/repository/impl/ImageLoaderImpl.kt
@@ -1,10 +1,14 @@
 package com.sergokuzneczow.repository.impl
 
 import android.content.Context
+import android.graphics.Bitmap
 import coil3.ImageLoader
 import coil3.disk.DiskCache
 import coil3.disk.directory
 import coil3.network.okhttp.OkHttpNetworkFetcherFactory
+import coil3.request.CachePolicy
+import coil3.request.ImageRequest
+import coil3.toBitmap
 import com.sergokuzneczow.repository.api.ImageLoaderApi
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
@@ -24,4 +28,18 @@ public class ImageLoaderImpl @Inject constructor(private val context: Context) :
         }.components {
             add(OkHttpNetworkFetcherFactory(callFactory = { okHttpClient }))
         }.build()
+
+    override suspend fun loadBitmap(path: String, onStart: () -> Unit, onSuccess: (Bitmap) -> Unit, onError: (Throwable) -> Unit) {
+        val request: ImageRequest = ImageRequest.Builder(context)
+            .data(path)
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .listener(
+                onStart = { onStart.invoke() },
+                onCancel = { },
+                onError = { imageRequest, errorResult -> onError.invoke(errorResult.throwable) },
+                onSuccess = { imageRequest, successResult -> onSuccess.invoke(successResult.image.toBitmap()) }
+            )
+            .build()
+        imageLoader.execute(request)
+    }
 }
