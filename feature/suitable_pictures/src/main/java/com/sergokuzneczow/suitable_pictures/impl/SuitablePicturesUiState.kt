e4e15734262094299fad241d5d58905c6a234f31
Sergo <sergokuzneczow@gmail.com>
2025-10-06 21:04:31 +0300

    Рефакторинг SuitablePicturesUiState.

--- feature/suitable_pictures/src/main/java/com/sergokuzneczow/suitable_pictures/impl/SuitablePicturesUiState.kt
+++ feature/suitable_pictures/src/main/java/com/sergokuzneczow/suitable_pictures/impl/SuitablePicturesUiState.kt
@@ -1,5 +1,6 @@
 package com.sergokuzneczow.suitable_pictures.impl
 
+import com.sergokuzneczow.domain.pager4.IPixelsPager4
 import com.sergokuzneczow.models.Picture
 import java.util.TreeMap
 
@@ -10,20 +11,21 @@ internal sealed interface SuitablePicturesUiState {
     data object Loading : SuitablePicturesUiState
 
     data class Success(val suitablePicturesPages: List<SuitablePicturesPage>) : SuitablePicturesUiState
+}
 
-    data class SuitablePicturesPage(
-        val items: List<SuitablePicture?>,
-    )
+internal data class SuitablePicturesPage(
+    val items: List<SuitablePicture?>,
+) {
     data class SuitablePicture(
         val pictureKey: String,
         val previewPath: String,
     )
 }
 
-internal fun List<Picture?>.toSuitablePictures(): List<SuitablePicturesUiState.SuitablePicture?> {
+internal fun List<Picture?>.toSuitablePictures(): List<SuitablePicturesPage.SuitablePicture?> {
     return this.map { picture ->
         picture?.let { pictureNotNull ->
-            SuitablePicturesUiState.SuitablePicture(
+            SuitablePicturesPage.SuitablePicture(
                 pictureKey = pictureNotNull.key,
                 previewPath = pictureNotNull.original,
             )
@@ -31,6 +33,6 @@ internal fun List<Picture?>.toSuitablePictures(): List<SuitablePicturesUiState.S
     }
 }
 
-internal fun TreeMap<Int, List<Picture?>>.toSuitablePicturesPages(): List<SuitablePicturesUiState.SuitablePicturesPage> {
-    return this.entries.map { SuitablePicturesUiState.SuitablePicturesPage(it.value.toSuitablePictures()) }
+internal fun TreeMap<Int, IPixelsPager4.Answer.Page<Picture?>>.toSuitablePicturesPages(): List<SuitablePicturesPage> {
+    return this.entries.map { SuitablePicturesPage(it.value.data.toSuitablePictures()) }
 }
