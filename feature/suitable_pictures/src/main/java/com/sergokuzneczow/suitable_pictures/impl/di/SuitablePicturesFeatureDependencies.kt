c2bab6f508202e75a8f51a926c8fb074424528bf
Sergo <sergokuzneczow@gmail.com>
2025-10-06 21:06:18 +0300

    Теперь SuitablePicturesFeatureDependencies не требует зависимость GetSuitablePicturesScreenPagerUseCase.

--- feature/suitable_pictures/src/main/java/com/sergokuzneczow/suitable_pictures/impl/di/SuitablePicturesFeatureDependencies.kt
+++ feature/suitable_pictures/src/main/java/com/sergokuzneczow/suitable_pictures/impl/di/SuitablePicturesFeatureDependencies.kt
@@ -3,16 +3,14 @@ package com.sergokuzneczow.suitable_pictures.impl.di
 import android.app.Application
 import android.content.Context
 import com.sergokuzneczow.domain.getPage.GetPage
+import com.sergokuzneczow.domain.get_suitable_pictures_screen_pager_use_case.GetSuitablePicturesScreenPager4UseCase
 import com.sergokuzneczow.domain.get_suitable_pictures_screen_pager_use_case.GetSuitablePicturesScreenPagerUseCase
 
 public interface SuitablePicturesFeatureDependencies {
-
-    public val getSuitablePicturesScreenPagerUseCase: GetSuitablePicturesScreenPagerUseCase
-
+    public val getSuitablePicturesScreenPager4UseCase: GetSuitablePicturesScreenPager4UseCase
     public val getPage: GetPage
 
     public interface Contract {
-
         public fun suitablePicturesFeatureDependenciesProvider(): SuitablePicturesFeatureDependencies
     }
 }
