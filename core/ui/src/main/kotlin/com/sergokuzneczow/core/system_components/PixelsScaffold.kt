d81f3341d0fdab39bab994e5f20cde07e642c7c8
Sergo <sergokuzneczow@gmail.com>
2025-10-07 17:56:51 +0300

    Добавил реализацию composable функции PixelsScaffold.

---
+++ core/ui/src/main/kotlin/com/sergokuzneczow/core/system_components/PixelsScaffold.kt
@@ -0,0 +1,30 @@
+package com.sergokuzneczow.core.system_components
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+public fun PixelsScaffold(
+    modifier: Modifier = Modifier,
+    topBar: @Composable () -> Unit = {},
+    bottomBar: @Composable () -> Unit = {},
+    snackbarHost: @Composable () -> Unit = {},
+    floatingActionButton: @Composable () -> Unit = {},
+    floatingActionButtonPosition: FabPosition = FabPosition.End,
+    content: @Composable (PaddingValues) -> Unit
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = topBar,
+        bottomBar = bottomBar,
+        snackbarHost = snackbarHost,
+        floatingActionButton = floatingActionButton,
+        floatingActionButtonPosition = floatingActionButtonPosition,
+        containerColor = Color.Transparent,
+        content = content,
+    )
+}
