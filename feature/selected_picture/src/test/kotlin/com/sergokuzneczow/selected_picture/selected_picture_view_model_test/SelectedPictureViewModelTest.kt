0edaa2b53fcde68190b069da04f0b4fd4e82b78d
Sergo <sergokuzneczow@gmail.com>
2025-10-14 19:56:37 +0300

    Добавил тест SelectedPictureViewModelTest для SelectedPictureViewModel.

--- feature/selected_picture/src/test/kotlin/com/sergokuzneczow/selected_picture/selected_picture_view_model_test/SelectedPictureViewModelTest.kt
+++ feature/selected_picture/src/test/kotlin/com/sergokuzneczow/selected_picture/selected_picture_view_model_test/SelectedPictureViewModelTest.kt
@@ -1,36 +1,39 @@
 package com.sergokuzneczow.selected_picture.selected_picture_view_model_test
 
-import com.sergokuzneczow.domain.get_picture_with_relations_case.GetPictureWithRelations2UseCase
-import com.sergokuzneczow.domain.get_picture_with_relations_case.GetPictureWithRelations2UseCase.Answer
-import com.sergokuzneczow.domain.get_picture_with_relations_case.GetPictureWithRelations2UseCase.Answer.AnswerState
-import com.sergokuzneczow.models.Picture
-import com.sergokuzneczow.models.PictureWithRelations
-import com.sergokuzneczow.repository.api.PictureRepositoryApi
-import kotlinx.coroutines.CoroutineScope
+import app.cash.turbine.test
+import com.google.common.truth.Truth.assertThat
+import com.sergokuzneczow.domain.get_picture_with_relations_2_use_case.GetPictureWithRelations2FakeUseCase
+import com.sergokuzneczow.selected_picture.impl.SelectedPictureIntent
+import com.sergokuzneczow.selected_picture.impl.SelectedPictureUiState
+import com.sergokuzneczow.selected_picture.impl.SelectedPictureUiState.Loading
+import com.sergokuzneczow.selected_picture.impl.SelectedPictureUiState.Success
+import com.sergokuzneczow.selected_picture.impl.view_model.SelectedPictureViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestResult
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
-import org.junit.runner.RunWith
-import org.mockito.Mockito.`when`
-import org.mockito.junit.MockitoJUnitRunner
-import org.mockito.kotlin.mock
+import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
-@RunWith(MockitoJUnitRunner::class)
 internal class SelectedPictureViewModelTest {
 
-    private val fakeGetPictureWithRelations2Case: FakeGetPictureWithRelations2UseCase = FakeGetPictureWithRelations2UseCase()
+    private lateinit var fakeGetPictureWithRelations2Case: GetPictureWithRelations2FakeUseCase
+
+    private lateinit var selectedPictureViewModel: SelectedPictureViewModel
 
     @Before
     fun beforeTest() {
         Dispatchers.setMain(StandardTestDispatcher())
+        fakeGetPictureWithRelations2Case = GetPictureWithRelations2FakeUseCase()
+        selectedPictureViewModel = SelectedPictureViewModel(
+            pictureKey = "",
+            getPictureWithRelations2UseCase = fakeGetPictureWithRelations2Case,
+        )
     }
 
     @After
@@ -38,62 +41,102 @@ internal class SelectedPictureViewModelTest {
         Dispatchers.resetMain()
     }
 
-    fun `should handle both success and error scenarios`() {
-        val cachedAnswer: Answer = createAnswer(pictureKey = "cached", answerState = AnswerState.CACHED)
-        val updatedAnswer: Answer = createAnswer(pictureKey = "updated", answerState = AnswerState.UPDATED)
+    @Test
+    fun `must handle cached data before new data`(): TestResult = runTest {
+        selectedPictureViewModel.uiState.test {
+            val loadingState: SelectedPictureUiState = awaitItem()
+            assertThat(loadingState).isInstanceOf(Loading::class.java)
 
+            val cachedKey = "cached"
+            fakeGetPictureWithRelations2Case.emitSuccess(cachedKey)
+            val successWithCachedData: SelectedPictureUiState = awaitItem()
+            assertThat(successWithCachedData).isInstanceOf(Success::class.java)
+            assertThat((successWithCachedData as Success).pictureKey).isEqualTo(cachedKey)
 
+            val newKey = "new"
+            fakeGetPictureWithRelations2Case.emitSuccess(newKey)
+            val successWithNewData: SelectedPictureUiState = awaitItem()
+            assertThat(successWithNewData).isInstanceOf(Success::class.java)
+            assertThat((successWithNewData as Success).pictureKey).isEqualTo(newKey)
+        }
     }
-}
 
-internal fun createAnswer(pictureKey: String, answerState: AnswerState): Answer {
-    return Answer(
-        data = PictureWithRelations(
-            picture = mock<Picture>().apply { `when`(this.key).thenReturn(pictureKey) },
-            tags = emptyList(),
-            colors = emptyList(),
-        ),
-        state = answerState,
-    )
-}
+    @Test
+    fun `handling an exception during the Loading state`(): TestResult = runTest {
+        selectedPictureViewModel.uiState.test {
+            val loadingState: SelectedPictureUiState = awaitItem()
+            assertThat(loadingState).isInstanceOf(Loading::class.java) // current state is Success
 
-internal class FakeGetPictureWithRelations2UseCase(
-    private val pictureRepository: PictureRepositoryApi = object : PictureRepositoryApi {
-        override fun getPicture(pictureKey: String): Flow<List<Picture>> {
-            TODO("Not yet implemented")
+            val message = "Message"
+            fakeGetPictureWithRelations2Case.emitFailure(message)
+            val loadingStateWithException: SelectedPictureUiState = awaitItem()
+            assertThat(loadingStateWithException).isInstanceOf(Loading::class.java)
+            assertThat(loadingStateWithException.exceptionMessage).isEqualTo(message)
         }
+    }
 
-        override fun getPictureWithRelation(pictureKey: String): Flow<PictureWithRelations?> {
-            TODO("Not yet implemented")
+    @Test
+    fun `handling an exception during the Success state`(): TestResult = runTest {
+        selectedPictureViewModel.uiState.test {
+            skipItems(1) // skip Loading state
+
+            val cachedKey = "cached"
+            fakeGetPictureWithRelations2Case.emitSuccess(cachedKey)
+            skipItems(1) // skip Success state
+
+            val message = "Exception message"
+            fakeGetPictureWithRelations2Case.emitFailure(message)
+            val successStateWithException: SelectedPictureUiState = awaitItem()
+            assertThat(successStateWithException).isInstanceOf(Success::class.java)
+            assertThat(successStateWithException.exceptionMessage).isEqualTo(message)
         }
+    }
 
-        override suspend fun getCachedPictureWithRelation(pictureKey: String): PictureWithRelations? {
-            TODO("Not yet implemented")
+    @Test
+    fun `exception message must be deleted after success answer`(): TestResult = runTest {
+        selectedPictureViewModel.uiState.test {
+            skipItems(1) // skip Loading state
+
+            val cachedKey = "cached"
+            fakeGetPictureWithRelations2Case.emitSuccess(cachedKey)
+            skipItems(1) // skip Success state
+
+            val message = "Exception message"
+            fakeGetPictureWithRelations2Case.emitFailure(message)
+            skipItems(1) // skip Success state with exception message
+
+            val newKey = "new"
+            fakeGetPictureWithRelations2Case.emitSuccess(newKey)
+            val successWithNewData: SelectedPictureUiState = awaitItem()
+            assertThat(successWithNewData).isInstanceOf(Success::class.java)
+            assertThat((successWithNewData as Success).pictureKey).isEqualTo(newKey)
+            assertThat((successWithNewData as Success).exceptionMessage).isEqualTo(null)
         }
+    }
 
-        override suspend fun getActualPictureWithRelation(pictureKey: String): PictureWithRelations {
-            TODO("Not yet implemented")
+    @Test
+    fun `handle intent CHANGE_VISIBLE_CURTAIN`(): TestResult = runTest {
+        selectedPictureViewModel.uiState.test {
+            skipItems(1) // skip Loading state
+
+            val cachedKey = "cached"
+            fakeGetPictureWithRelations2Case.emitSuccess(cachedKey)
+            val successState: SelectedPictureUiState = awaitItem()
+            assertThat(successState).isInstanceOf(Success::class.java)
+            assertThat((successState as Success).curtainVisible).isFalse()
+            assertThat(successState.infoFabVisible).isTrue()
+
+            selectedPictureViewModel.setIntent(SelectedPictureIntent.CHANGE_VISIBLE_CURTAIN)
+            val curtainIsVisible: SelectedPictureUiState = awaitItem()
+            assertThat(curtainIsVisible).isInstanceOf(Success::class.java)
+            assertThat((curtainIsVisible as Success).curtainVisible).isTrue()
+            assertThat(curtainIsVisible.infoFabVisible).isFalse()
+
+            selectedPictureViewModel.setIntent(SelectedPictureIntent.CHANGE_VISIBLE_CURTAIN)
+            val curtainIsHide: SelectedPictureUiState = awaitItem()
+            assertThat(curtainIsHide).isInstanceOf(Success::class.java)
+            assertThat((curtainIsHide as Success).curtainVisible).isFalse()
+            assertThat(curtainIsHide.infoFabVisible).isTrue()
         }
-
-        override suspend fun cachingPictureWithRelation(pictureWithRelations: PictureWithRelations) {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun updatePicture(pictureKey: String) {
-            TODO("Not yet implemented")
-        }
-
-        override suspend fun updatePictureWithRelation(pictureKey: String) {
-            TODO("Not yet implemented")
-        }
-    },
-) : GetPictureWithRelations2UseCase(pictureRepository) {
-
-    internal val dataSource: MutableSharedFlow<Result<Answer>> = MutableSharedFlow()
-
-    override fun execute(scope: CoroutineScope, pictureKey: String): Flow<Result<Answer>> = dataSource
-
-    internal fun emitData(scope: CoroutineScope, data: Result<Answer>) {
-        scope.launch { dataSource.emit(data) }
     }
 }
