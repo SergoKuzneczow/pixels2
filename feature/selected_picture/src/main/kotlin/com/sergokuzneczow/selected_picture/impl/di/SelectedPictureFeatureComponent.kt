342fa19d4f323c8a98396216a7cb013895c92e64
Sergo <sergokuzneczow@gmail.com>
2025-10-14 12:33:55 +0300

    Теперь SelectedPictureFeatureComponent предоставляет object Instance для получения экземпляра dagger компонента.

--- feature/selected_picture/src/main/kotlin/com/sergokuzneczow/selected_picture/impl/di/SelectedPictureFeatureComponent.kt
+++ feature/selected_picture/src/main/kotlin/com/sergokuzneczow/selected_picture/impl/di/SelectedPictureFeatureComponent.kt
@@ -1,6 +1,7 @@
 package com.sergokuzneczow.selected_picture.impl.di
 
-import com.sergokuzneczow.selected_picture.impl.SelectedPictureViewModel
+import com.sergokuzneczow.selected_picture.impl.view_model.SelectedPictureViewModel
+import com.sergokuzneczow.selected_picture.impl.view_model.SelectedPictureViewModelFactory
 import dagger.Component
 
 @Component(
@@ -8,11 +9,31 @@ import dagger.Component
 )
 internal interface SelectedPictureFeatureComponent {
 
-    fun inject(model:SelectedPictureViewModel)
+    fun inject(model: SelectedPictureViewModel)
+
+    fun inject(model: SelectedPictureViewModelFactory)
 
     @Component.Builder
     interface Builder {
         fun setDependencties(d: SelectedPictureFeatureDependencies): Builder
         fun build(): SelectedPictureFeatureComponent
+    }
+
+    object Instance {
+
+        private var selectedPictureFeatureComponent: SelectedPictureFeatureComponent? = null
+
+        internal fun get(d: SelectedPictureFeatureDependencies): SelectedPictureFeatureComponent? {
+            if (selectedPictureFeatureComponent == null) {
+                selectedPictureFeatureComponent = DaggerSelectedPictureFeatureComponent.builder()
+                    .setDependencties(d)
+                    .build()
+            }
+            return selectedPictureFeatureComponent
+        }
+
+        internal fun clear() {
+            selectedPictureFeatureComponent = null
+        }
     }
 }
