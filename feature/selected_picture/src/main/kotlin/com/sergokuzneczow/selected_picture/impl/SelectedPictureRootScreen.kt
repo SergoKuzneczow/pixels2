c7e923620bac06223ced18f9e0d936030682a166
Sergo <sergokuzneczow@gmail.com>
2025-10-14 12:36:40 +0300

    SelectedPictureRootScreen теперь использует методы SelectedPictureViewModelFactory() для фабричного создания экземпляра SelectedPictureViewModel.

--- feature/selected_picture/src/main/kotlin/com/sergokuzneczow/selected_picture/impl/SelectedPictureRootScreen.kt
+++ feature/selected_picture/src/main/kotlin/com/sergokuzneczow/selected_picture/impl/SelectedPictureRootScreen.kt
@@ -7,14 +7,17 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sergokuzneczow.selected_picture.impl.SelectedPictureIntent.CHANGE_VISIBLE_CURTAIN
 import com.sergokuzneczow.selected_picture.impl.ui.SelectedPictureScreen
+import com.sergokuzneczow.selected_picture.impl.view_model.SelectedPictureViewModel
+import com.sergokuzneczow.selected_picture.impl.view_model.SelectedPictureViewModelFactory
 
 @Composable
 internal fun SelectedPictureRootScreen(
     pictureKey: String,
     navigateToBottomSheetPictureInfoDestination: (pictureKey: String) -> Unit,
 ) {
-    val vm: SelectedPictureViewModel = viewModel(factory = SelectedPictureViewModel.Factory(LocalContext.current, pictureKey))
+    val vm: SelectedPictureViewModel = viewModel(factory = SelectedPictureViewModelFactory(LocalContext.current, pictureKey))
     val uiState: SelectedPictureUiState by vm.uiState.collectAsStateWithLifecycle()
+
     SelectedPictureScreen(
         uiState = uiState,
         changeCurtainVisible = { vm.setIntent(CHANGE_VISIBLE_CURTAIN) },
