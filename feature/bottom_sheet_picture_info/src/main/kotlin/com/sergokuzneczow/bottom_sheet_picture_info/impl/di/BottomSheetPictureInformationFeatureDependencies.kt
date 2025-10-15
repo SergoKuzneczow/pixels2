8e918a0f933eca2cc9a516c625785a655a6a9f49
Sergo <sergokuzneczow@gmail.com>
2025-10-15 19:16:29 +0300

    BottomSheetPictureInformationFeatureDependencies больше не требуте зависимость GetPictureWithRelationsUseCase.

--- feature/bottom_sheet_picture_info/src/main/kotlin/com/sergokuzneczow/bottom_sheet_picture_info/impl/di/BottomSheetPictureInformationFeatureDependencies.kt
+++ feature/bottom_sheet_picture_info/src/main/kotlin/com/sergokuzneczow/bottom_sheet_picture_info/impl/di/BottomSheetPictureInformationFeatureDependencies.kt
@@ -3,12 +3,13 @@ package com.sergokuzneczow.bottom_sheet_picture_info.impl.di
 import android.app.Application
 import android.content.Context
 import com.sergokuzneczow.domain.get_first_page_key_use_case.GetFirstPageKeyUseCase
+import com.sergokuzneczow.domain.get_picture_with_relations_2_use_case.GetPictureWithRelations2UseCase
 import com.sergokuzneczow.domain.get_picture_with_relations_use_case.GetPictureWithRelationsCase
 import com.sergokuzneczow.repository.api.ImageLoaderApi
 import com.sergokuzneczow.repository.api.StorageRepositoryApi
 
 public interface BottomSheetPictureInformationFeatureDependencies {
-    public val getPictureWithRelationsCase: GetPictureWithRelationsCase
+    public val getPictureWithRelations2UseCase: GetPictureWithRelations2UseCase
     public val getFirstPageKeyUseCase: GetFirstPageKeyUseCase
     public val imageLoaderApi: ImageLoaderApi
     public val storageRepositoryApi: StorageRepositoryApi
