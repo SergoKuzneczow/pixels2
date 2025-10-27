43819f1201a94e3d3388ad2583d6ecb148c13893
Sergo <sergokuzneczow@gmail.com>
2025-10-27 18:56:04 +0300

    Теперь MainActivityViewModelFactory не запрашивает зависимость PictureSavingServiceProviderApi.

--- app/src/main/kotlin/com/sergokuzneczow/pixels2/view_model/MainActivityViewModelFactory.kt
+++ app/src/main/kotlin/com/sergokuzneczow/pixels2/view_model/MainActivityViewModelFactory.kt
@@ -4,10 +4,10 @@ import android.content.Context
 import androidx.annotation.NonUiContext
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.sergokuzneczow.domain.picture_load_and_save_use_case.LoadAndSavePictureUseCase
 import com.sergokuzneczow.pixels2.di.applicationComponent
 import com.sergokuzneczow.repository.api.NetworkMonitorApi
 import com.sergokuzneczow.repository.api.SettingsRepositoryApi
-import com.sergokuzneczow.service_save_picture.api.PictureSavingServiceProviderApi
 import javax.inject.Inject
 
 internal class MainActivityViewModelFactory(@NonUiContext context: Context) : ViewModelProvider.Factory {
@@ -16,10 +16,10 @@ internal class MainActivityViewModelFactory(@NonUiContext context: Context) : Vi
     lateinit var networkMonitorApi: NetworkMonitorApi
 
     @Inject
-    lateinit var savePictureServiceProvider: PictureSavingServiceProviderApi
+    lateinit var settingsRepositoryApi: SettingsRepositoryApi
 
     @Inject
-    lateinit var settingsRepositoryApi: SettingsRepositoryApi
+    lateinit var loadAndSavePictureUseCase: LoadAndSavePictureUseCase
 
     init {
         context.applicationComponent.inject(this)
@@ -29,8 +29,8 @@ internal class MainActivityViewModelFactory(@NonUiContext context: Context) : Vi
         if (modelClass.isAssignableFrom(MainActivityViewModel::class.java)) {
             return MainActivityViewModel(
                 networkMonitorApi = networkMonitorApi,
-                savePictureServiceProvider = savePictureServiceProvider,
                 settingsRepositoryApi = settingsRepositoryApi,
+                loadAndSavePictureUseCase = loadAndSavePictureUseCase,
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
