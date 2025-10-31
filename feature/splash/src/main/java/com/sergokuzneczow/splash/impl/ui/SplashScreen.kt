34841643055593c2656289fe840f65f2295bf6c0
Sergo <sergokuzneczow@gmail.com>
2025-10-31 16:56:25 +0300

    Заменил PixelsCircularProgressIndicator на PixelsProgressIndicator для SplashScreen.

--- feature/splash/src/main/java/com/sergokuzneczow/splash/impl/ui/SplashScreen.kt
+++ feature/splash/src/main/java/com/sergokuzneczow/splash/impl/ui/SplashScreen.kt
@@ -1,31 +1,12 @@
 package com.sergokuzneczow.splash.impl.ui
 
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.VectorConverter
-import androidx.compose.animation.core.animateValue
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavOptions
 import androidx.navigation.navOptions
 import com.sergokuzneczow.core.system_components.progress_indicators.PixelsProgressIndicator
-import com.sergokuzneczow.core.ui.PixelsIcons
-import com.sergokuzneczow.core.ui.PixelsTheme
-import com.sergokuzneczow.core.utilites.ThemePreviews
 import com.sergokuzneczow.models.ApplicationSettings
 import com.sergokuzneczow.splash.api.SplashScreenRoute
 import com.sergokuzneczow.splash.impl.SplashScreenUiState
@@ -39,10 +20,7 @@ internal fun SplashScreen(
 ) {
     Box(modifier = Modifier.fillMaxSize()) {
         when (uiState) {
-
-            SplashScreenUiState.Loading -> {
-                PixelsProgressIndicator()
-            }
+            SplashScreenUiState.Loading -> PixelsProgressIndicator()
 
             SplashScreenUiState.Finish -> {
                 val navOptions: NavOptions = navOptions {
