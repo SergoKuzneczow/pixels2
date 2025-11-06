5f0953e08e08dce70d3d90b1ed0729800b9e1493
Sergo <sergokuzneczow@gmail.com>
2025-11-06 22:50:33 +0300

    Рефакторинг composable функций PixelsSurfaceButton и PixelsPrimaryButton.

--- core/ui/src/main/kotlin/com/sergokuzneczow/core/system_components/buttons/PixelsPrimaryButton.kt
+++ core/ui/src/main/kotlin/com/sergokuzneczow/core/system_components/buttons/PixelsPrimaryButton.kt
@@ -1,13 +1,19 @@
 package com.sergokuzneczow.core.system_components.buttons
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,14 +21,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.sergokuzneczow.core.ui.Dimensions
+import com.sergokuzneczow.core.ui.PixelsIcons
 import com.sergokuzneczow.core.ui.PixelsTheme
-import com.sergokuzneczow.core.utilites.ThemePreviews
+import com.sergokuzneczow.core.utilites.ThemeUiPreviews
+
+private val BUTTON_HEIGHT: Dp = 40.dp
+private val BUTTON_ICON_SIZE: Dp = 16.dp
+private val BORDER_WIDTH: Dp = 1.dp
 
 @Composable
 public fun PixelsPrimaryButton(
@@ -31,21 +44,26 @@ public fun PixelsPrimaryButton(
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
     contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    icon: ImageVector? = null,
+    iconContent: (@Composable () -> Unit)? = null,
     interactionSource: MutableInteractionSource? = null,
 ) {
     val interactionSource = interactionSource ?: remember { MutableInteractionSource() }
     Surface(
         onClick = onClick,
-        modifier = modifier.semantics { role = Role.Button },
+        modifier = modifier
+            .height(BUTTON_HEIGHT)
+            .semantics { role = Role.Button },
         enabled = enabled,
-        shape = Dimensions.PixelsShape,
+        shape = Dimensions.ButtonCornerShape,
         color = MaterialTheme.colorScheme.primary,
         contentColor = MaterialTheme.colorScheme.onPrimary,
-        shadowElevation = 1.dp,
+        border = BorderStroke(BORDER_WIDTH, MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f)),
+        shadowElevation = 4.dp,
         interactionSource = interactionSource
     ) {
         Row(
-            Modifier
+            modifier = Modifier
                 .defaultMinSize(
                     minWidth = ButtonDefaults.MinWidth,
                     minHeight = ButtonDefaults.MinHeight
@@ -54,28 +72,49 @@ public fun PixelsPrimaryButton(
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically,
             content = {
+                icon?.let {
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onPrimary,
+                        modifier = Modifier
+                            .size(BUTTON_ICON_SIZE)
+                            .align(Alignment.CenterVertically)
+                    )
+                    Spacer(modifier = Modifier.width(Dimensions.LargePadding))
+                }
+                iconContent?.let {
+                    it.invoke()
+                    Spacer(modifier = Modifier.width(Dimensions.LargePadding))
+                }
                 Text(
                     text = text,
                     style = MaterialTheme.typography.bodyMedium,
                     color = MaterialTheme.colorScheme.onPrimary,
                     textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .align(Alignment.CenterVertically)
+                    maxLines = 1,
+                    modifier = Modifier.align(Alignment.CenterVertically)
                 )
             }
         )
     }
 }
 
-@ThemePreviews
+@ThemeUiPreviews
 @Composable
-private fun PixelsPrimaryButtonPreview() {
+private fun PixelsPrimaryButtonUiPreview() {
     PixelsTheme {
-        Box {
-            PixelsPrimaryButton(
-                text = "Preview",
-                onClick = {},
-            )
+        Surface {
+            Box {
+                PixelsPrimaryButton(
+                    text = "Button preview",
+                    onClick = {},
+                    icon = PixelsIcons.selector,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(24.dp),
+                )
+            }
         }
     }
 }
--- core/ui/src/main/kotlin/com/sergokuzneczow/core/system_components/buttons/PixelsSurfaceButton.kt
+++ core/ui/src/main/kotlin/com/sergokuzneczow/core/system_components/buttons/PixelsSurfaceButton.kt
@@ -1,12 +1,17 @@
 package com.sergokuzneczow.core.system_components.buttons
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -21,10 +26,16 @@ import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.sergokuzneczow.core.ui.Dimensions
+import com.sergokuzneczow.core.ui.PixelsIcons
 import com.sergokuzneczow.core.ui.PixelsTheme
 import com.sergokuzneczow.core.utilites.ThemePreviews
+
+private val BUTTON_HEIGHT: Dp = 40.dp
+private val BUTTON_ICON_SIZE: Dp = 16.dp
+private val BORDER_WIDTH: Dp = 1.dp
 
 @Composable
 public fun PixelsSurfaceButton(
@@ -33,57 +44,59 @@ public fun PixelsSurfaceButton(
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
     contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
-    interactionSource: MutableInteractionSource? = null,
     icon: ImageVector? = null,
-    iconContent: @Composable () -> Unit = {}
+    iconContent: (@Composable () -> Unit)? = null,
+    interactionSource: MutableInteractionSource? = null,
 ) {
     val interactionSource = interactionSource ?: remember { MutableInteractionSource() }
     Surface(
         onClick = onClick,
-        modifier = modifier.semantics { role = Role.Button },
+        modifier = modifier
+            .height(BUTTON_HEIGHT)
+            .semantics { role = Role.Button },
         enabled = enabled,
-        shape = Dimensions.PixelsShape,
-        color = MaterialTheme.colorScheme.surfaceContainer,
+        shape = Dimensions.ButtonCornerShape,
+        color = MaterialTheme.colorScheme.surface,
         contentColor = MaterialTheme.colorScheme.onSurface,
-        border = Dimensions.Border,
-        shadowElevation = 1.dp,
+        border = BorderStroke(BORDER_WIDTH, MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f)),
+        shadowElevation = 4.dp,
         interactionSource = interactionSource
     ) {
-        Box {
-            Row(
-                content = {
-                    icon?.let {
-                        Icon(
-                            imageVector = it,
-                            contentDescription = null,
-                            modifier = Modifier.size(16.dp)
-                        )
-                    }
-                    iconContent.invoke()
-                },
-                modifier = Modifier
-                    .padding(start = Dimensions.Padding)
-                    .align(Alignment.CenterStart)
-            )
-            Row(
-                Modifier
-                    .defaultMinSize(
-                        minWidth = ButtonDefaults.MinWidth,
-                        minHeight = ButtonDefaults.MinHeight
+        Row(
+            modifier = Modifier
+                .defaultMinSize(
+                    minWidth = ButtonDefaults.MinWidth,
+                    minHeight = ButtonDefaults.MinHeight
+                )
+                .padding(contentPadding),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            content = {
+                icon?.let {
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier
+                            .size(BUTTON_ICON_SIZE)
+                            .align(Alignment.CenterVertically)
                     )
-                    .padding(contentPadding)
-                    .align(Alignment.Center),
-                content = {
-                    Text(
-                        text = text,
-                        style = MaterialTheme.typography.bodyMedium,
-                        color = MaterialTheme.colorScheme.onSurface,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.align(Alignment.CenterVertically)
-                    )
+                    Spacer(modifier = Modifier.width(Dimensions.LargePadding))
                 }
-            )
-        }
+                iconContent?.let {
+                    it.invoke()
+                    Spacer(modifier = Modifier.width(Dimensions.LargePadding))
+                }
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    textAlign = TextAlign.Center,
+                    maxLines = 1,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
+        )
     }
 }
 
@@ -91,11 +104,17 @@ public fun PixelsSurfaceButton(
 @Composable
 private fun PixelsSurfaceButtonPreview() {
     PixelsTheme {
-        Box {
-            PixelsSurfaceButton(
-                text = "Preview",
-                onClick = {},
-            )
+        Surface {
+            Box {
+                PixelsSurfaceButton(
+                    text = "Button preview",
+                    onClick = {},
+                    icon = PixelsIcons.selector,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(24.dp),
+                )
+            }
         }
     }
 }
