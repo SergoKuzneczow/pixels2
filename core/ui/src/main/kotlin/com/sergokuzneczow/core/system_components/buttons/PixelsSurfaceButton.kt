a6a9083aeafc55a44c5014a74ba6afee7afd04c5
Sergo <sergokuzneczow@gmail.com>
2025-11-09 12:24:52 +0300

    Теперь PixelsSurfaceButton корректно отображает иконку для кнопки, а так же по умолчанию предоставляет возмонжсоть менять состояние видимости progress bar.

--- core/ui/src/main/kotlin/com/sergokuzneczow/core/system_components/buttons/PixelsSurfaceButton.kt
+++ core/ui/src/main/kotlin/com/sergokuzneczow/core/system_components/buttons/PixelsSurfaceButton.kt
@@ -1,5 +1,8 @@
 package com.sergokuzneczow.core.system_components.buttons
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -7,11 +10,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -28,13 +31,15 @@ import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.sergokuzneczow.core.system_components.progress_indicators.PixelsProgressIndicator
 import com.sergokuzneczow.core.ui.Dimensions
 import com.sergokuzneczow.core.ui.PixelsIcons
 import com.sergokuzneczow.core.ui.PixelsTheme
 import com.sergokuzneczow.core.utilites.ThemePreviews
 
 private val BUTTON_HEIGHT: Dp = 40.dp
-private val BUTTON_ICON_SIZE: Dp = 16.dp
+private val BUTTON_HORIZONTAL_PADDING: Dp = 24.dp
+private val ICON_SIZE: Dp = 16.dp
 private val BORDER_WIDTH: Dp = 1.dp
 
 @Composable
@@ -45,8 +50,9 @@ public fun PixelsSurfaceButton(
     enabled: Boolean = true,
     contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
     icon: ImageVector? = null,
-    iconContent: (@Composable () -> Unit)? = null,
+    iconContent: (@Composable (Modifier) -> Unit)? = null,
     interactionSource: MutableInteractionSource? = null,
+    isVisibleProgress: Boolean = false,
 ) {
     val interactionSource = interactionSource ?: remember { MutableInteractionSource() }
     Surface(
@@ -56,45 +62,54 @@ public fun PixelsSurfaceButton(
             .semantics { role = Role.Button },
         enabled = enabled,
         shape = Dimensions.ButtonCornerShape,
-        color = MaterialTheme.colorScheme.surface,
+        color = MaterialTheme.colorScheme.surfaceContainer,
         contentColor = MaterialTheme.colorScheme.onSurface,
         border = BorderStroke(BORDER_WIDTH, MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f)),
-        shadowElevation = 4.dp,
+        shadowElevation = 2.dp,
         interactionSource = interactionSource
     ) {
         Row(
-            modifier = Modifier
-                .defaultMinSize(
-                    minWidth = ButtonDefaults.MinWidth,
-                    minHeight = ButtonDefaults.MinHeight
-                )
-                .padding(contentPadding),
+            modifier = Modifier.height(BUTTON_HEIGHT),
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically,
             content = {
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.widthIn(min = BUTTON_HORIZONTAL_PADDING),
+                    content = {
+                        AnimatedVisibility(
+                            visible = isVisibleProgress,
+                            enter = fadeIn(),
+                            exit = fadeOut(),
+                            modifier = Modifier,
+                            content = {
+                                PixelsProgressIndicator(Dimensions.VerySmallProgressBarSize)
+                                Spacer(modifier = Modifier.width(8.dp))
+                            }
+                        )
+                    }
+                )
+
                 icon?.let {
                     Icon(
                         imageVector = icon,
                         contentDescription = null,
                         tint = MaterialTheme.colorScheme.onSurface,
-                        modifier = Modifier
-                            .size(BUTTON_ICON_SIZE)
-                            .align(Alignment.CenterVertically)
+                        modifier = Modifier.size(ICON_SIZE)
                     )
-                    Spacer(modifier = Modifier.width(Dimensions.LargePadding))
+                    Spacer(modifier = Modifier.width(8.dp))
                 }
-                iconContent?.let {
-                    it.invoke()
-                    Spacer(modifier = Modifier.width(Dimensions.LargePadding))
-                }
+
                 Text(
                     text = text,
                     style = MaterialTheme.typography.bodyMedium,
                     color = MaterialTheme.colorScheme.onSurface,
                     textAlign = TextAlign.Center,
                     maxLines = 1,
-                    modifier = Modifier.align(Alignment.CenterVertically)
                 )
+
+                Spacer(modifier = Modifier.widthIn(min = BUTTON_HORIZONTAL_PADDING))
             }
         )
     }
@@ -102,7 +117,7 @@ public fun PixelsSurfaceButton(
 
 @ThemePreviews
 @Composable
-private fun PixelsSurfaceButtonPreview() {
+private fun PixelsSurfaceButtonWithIconPreview() {
     PixelsTheme {
         Surface {
             Box {
@@ -110,6 +125,26 @@ private fun PixelsSurfaceButtonPreview() {
                     text = "Button preview",
                     onClick = {},
                     icon = PixelsIcons.selector,
+                    isVisibleProgress = true,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(24.dp),
+                )
+            }
+        }
+    }
+}
+
+@ThemePreviews
+@Composable
+private fun PixelsSurfaceButtonWithoutIconPreview() {
+    PixelsTheme {
+        Surface {
+            Box {
+                PixelsSurfaceButton(
+                    text = "Button preview",
+                    onClick = {},
+                    icon = null,
                     modifier = Modifier
                         .align(Alignment.Center)
                         .padding(24.dp),
