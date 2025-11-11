8d5212ba668f5e5cb3449fa9b63e43ac40273d31
Sergo <sergokuzneczow@gmail.com>
2025-11-11 13:57:05 +0300

    Изменил HomeListUiState. Теперь состояние Loading не содержит данных. Добавлено новое состояние OpenSelectedQuery.

--- feature/home/src/main/kotlin/com/sergokuzneczow/home/impl/HomeListUiState.kt
+++ feature/home/src/main/kotlin/com/sergokuzneczow/home/impl/HomeListUiState.kt
@@ -5,19 +5,17 @@ import com.sergokuzneczow.home.impl.models.SuggestedQueriesPage
 
 internal sealed interface HomeListUiState {
 
-    class Loading private constructor(
-        val standardQuery: List<StandardQuery>,
-    ) : HomeListUiState {
-        constructor() : this(standardQuery = StandardQuery.standardQueries)
-    }
+    data object Loading : HomeListUiState
 
     class Success private constructor(
         val standardQuery: List<StandardQuery>,
-        val suggestedQueriesPages: List<SuggestedQueriesPage>,
+        val suggestedQueriesPages: List<SuggestedQueriesPage>?,
     ) : HomeListUiState {
-        constructor(suggestedQueriesPages: List<SuggestedQueriesPage>) : this(
+        constructor(suggestedQueriesPages: List<SuggestedQueriesPage>?) : this(
             standardQuery = StandardQuery.standardQueries,
             suggestedQueriesPages = suggestedQueriesPages
         )
     }
+
+    data class OpenSelectedQuery(val pageKey: Long) : HomeListUiState
 }
