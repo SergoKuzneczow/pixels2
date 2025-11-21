6496e70fdfe3dc71fe8e06bcdbd6b5f103550376
Sergo <sergokuzneczow@gmail.com>
2025-11-21 14:06:21 +0300

    Изменил модуль datastore_picture. Теперь StoragePictureImpl.saveBitmap() способен сохранять изображение для api < 29.

--- core/datastore_picture/src/main/java/com/sergokuzneczow/datastore_picture/impl/StoragePictureImpl.kt
+++ core/datastore_picture/src/main/java/com/sergokuzneczow/datastore_picture/impl/StoragePictureImpl.kt
@@ -8,41 +8,48 @@ import android.net.Uri
 import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
-import androidx.annotation.RequiresApi
 import com.sergokuzneczow.datastore_picture.api.StoragePictureApi
 import jakarta.inject.Inject
 import java.io.ByteArrayOutputStream
+import java.io.File
 import java.io.OutputStream
 import java.util.UUID
 
 public class StoragePictureImpl @Inject constructor(
-    private val context: Context
+    private val context: Context,
 ) : StoragePictureApi {
     private companion object {
 
-        const val APPLICATION_FOLDER_NAME: String = "Pixels"
+        const val APPLICATION_FOLDER_NAME: String = "Pixels2"
     }
 
     override fun savePictureBitmapToPictureDir(bitmap: Bitmap, onStart: () -> Unit, onSuccess: (uri: Uri) -> Unit, onFailure: (e: Exception) -> Unit) {
-        when {
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> saveWhenApiMoreOrEqualsQ(bitmap, onStart, onSuccess, onFailure)
-//            else -> MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-            else -> throw IllegalStateException("Application can't saved picture when device runs on api<29.")
-        }
+        saveBitmap(bitmap, onStart, onSuccess, onFailure)
     }
 
-    @RequiresApi(Build.VERSION_CODES.Q)
-    private fun saveWhenApiMoreOrEqualsQ(bitmap: Bitmap, onStart: () -> Unit, onSuccess: (uri: Uri) -> Unit, onFailure: (e: Exception) -> Unit) {
+    private fun saveBitmap(
+        bitmap: Bitmap,
+        onStart: () -> Unit,
+        onSuccess: (uri: Uri) -> Unit,
+        onFailure: (e: Exception) -> Unit,
+    ) {
         onStart.invoke()
-        val contentValues = ContentValues().apply {
+
+        val collectionUri: Uri = when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+            else -> MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+
+        val contentValues: ContentValues = ContentValues().apply {
             put(MediaStore.MediaColumns.DISPLAY_NAME, UUID.randomUUID().toString())
             put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-            put(MediaStore.MediaColumns.RELATIVE_PATH, "${Environment.DIRECTORY_PICTURES}/$APPLICATION_FOLDER_NAME")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) put(MediaStore.MediaColumns.RELATIVE_PATH, "${Environment.DIRECTORY_PICTURES}${File.pathSeparator}${APPLICATION_FOLDER_NAME}")
         }
-        val collectionUri: Uri = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
         val resolver: ContentResolver = context.contentResolver
         val uri: Uri? = resolver.insert(collectionUri, contentValues)
+
         val outputStream: OutputStream? = uri?.let { resolver.openOutputStream(it) }
+
         try {
             outputStream?.apply {
                 write(bitmapToByteArray(bitmap))
