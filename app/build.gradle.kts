0122791ff223563f8bbb44f697f9b00b64775f17
Sergo <sergokuzneczow@gmail.com>
2025-11-25 14:51:39 +0300

    Изменил версию приложения.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sergokuzneczow.pixels2"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "0.3"
     }
 
     buildTypes {
