490478eeef409838b5a7a4486503afd0ddcc16bb
Sergo <sergokuzneczow@gmail.com>
2026-01-19 17:35:53 +0300

    Удалил модуль base.

--- feature/search_suitable_pictures/src/main/kotlin/com/sergokuzneczow/search_suitable_pictures/impl/di/SearchSuitablePicturesDependencies.kt
+++
@@ -1,7 +0,0 @@
-package com.sergokuzneczow.search_suitable_pictures.impl.di
-
-import android.app.Application
-import android.content.Context
-import com.sergokuzneczow.repository.api.PageRepositoryApi
-
-
